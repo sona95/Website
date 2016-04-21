94e83ffb8876945c0a49253d0f28a8f67175c7f6
sona95 <unusyansona@gmail.com>
2016-04-21 17:02:45 +0400

    New_Website

---
+++ js/script.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Sona Unusyan on 4/21/2016.
+ */
+$(document).ready(function(){
+    $('.bxslider').bxSlider({
+        slideWidth: 400,
+        minSlides: 3,
+        maxSlides: 3,
+        slideMargin: 10,
+        pager: false,
+        controls: false,
+        auto: true,
+        pause: 3000,
+        moveSlides: 1,
+        autoHover:true
+    });
+});
